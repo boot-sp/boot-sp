ebc6f7f326f839cbb10e75b9cc4e135dd5b039e9
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-08-12 17:18:21 -0700

    drop tree fct

--- examples/schultz/unique_schultz.py
+++ examples/schultz/unique_schultz.py
@@ -93,27 +93,6 @@ def kw_creator(cfg):
     kwargs = {"num_scens" : cfg.get('num_scens', None)}
     return kwargs
 
-def sample_tree_scen_creator(sname, stage, sample_branching_factors, seed,
-                             given_scenario=None, **scenario_creator_kwargs):
-    """ Create a scenario within a sample tree. Mainly for multi-stage and simple for two-stage.
-        (this function supports zhat and confidence interval code)
-    Args:
-        sname (string): scenario name to be created
-        stage (int >=1 ): for stages > 1, fix data based on sname in earlier stages
-        sample_branching_factors (list of ints): branching factors for the sample tree
-        seed (int): To allow random sampling (for some problems, it might be scenario offset)
-        given_scenario (Pyomo concrete model): if not None, use this to get data for ealier stages
-        scenario_creator_kwargs (dict): keyword args for the standard scenario creator funcion
-    Returns:
-        scenario (Pyomo concrete model): A scenario for sname with data in stages < stage determined
-                                         by the arguments
-    """
-    # Since this is a two-stage problem, we don't have to do much.
-    sca = scenario_creator_kwargs.copy()
-    sca["seedoffset"] = seed
-    sca["num_scens"] = sample_branching_factors[0]  # two-stage problem
-    return scenario_creator(sname, **sca)
-
 
 #============================
 def scenario_denouement(rank, scenario_name, scenario):
